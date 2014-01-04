d6ba506cc4483f8f43675e84f1dfe28630cb2c40
dpatekar <dpatekar@gmail.com>
2014-01-04 17:24:57 +0100

    Init

---
+++ MCU/MCU.ino
@@ -0,0 +1,117 @@
+#include <aes.h>
+#define blockSize 4096
+
+enum workMode {
+  enc,
+  dec
+};
+byte key[] = 
+{
+  0x64, 0x34, 0x90, 0x67, 0x01, 0x31, 0x20, 0x11, 0x80, 0x67, 0x03, 0x43, 0x99, 0x77, 0x22, 0x34,
+  0x86, 0x61, 0x18, 0x23, 0x07, 0x04, 0x76, 0x18, 0x69, 0x08, 0x99, 0xF7, 0xA4, 0xC4, 0xD4, 0xEE
+} 
+;
+byte encsw[] =
+{
+  0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+} 
+;
+byte decsw[] =
+{
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+} 
+;
+
+byte inBuffer[blockSize], tempiBuffer[16], outBuffer[16], lenBuffer[4], iv[16] ;
+uint len;
+workMode mode;
+
+aes_context ac;
+
+void setup() {
+  Serial.begin(115200);
+  //mijenja ponasanje Serial.readBytes - bez cekanja
+  Serial.setTimeout(0);
+  aes_set_key(key, 32, &ac);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {      
+  //ceka do pojave 32 byte i provjerava da li se radi o switch-u
+  while (Serial.available() < 32){};  
+  Serial.readBytes((char *)inBuffer, 32);
+  if (memcmp(inBuffer, encsw, 32) == 0){    
+    mode = enc;      
+  }
+  else if (memcmp(inBuffer, decsw, 32) == 0){
+    mode = dec; 
+  }
+  else{
+    //primljen je podatak koji nije switch pa se prazni serial buffer i prekida s izvršavanjem trenutnog loop-a
+    Serial.flush();
+    return; 
+  }
+  
+  digitalWrite(13, HIGH);
+  
+  //citanje duljine dolazne datoteke
+  while (Serial.available() < 4){};  
+  Serial.readBytes((char *)lenBuffer, 4);
+  len = *(uint*)lenBuffer; 
+  
+  int incomingBlocks = (len % 4096 != 0) ? ((len / 4096) + 1) : (len / 4096);
+  
+  if (mode == enc){  
+    //generiranje IV-a
+    for(size_t i = 0; i < 16; i++)
+      iv[i] = rand() % 256;
+
+    Serial.write(lenBuffer, 4);
+    Serial.write(iv, 16);            
+  }
+  else if (mode == dec){
+    //citanje IV-a
+    while (Serial.available() < 16){};  
+    Serial.readBytes((char *)iv, 16);
+  }
+
+  int totalReceived, iterReceived, receivedBlocks = 0;
+
+  while (receivedBlocks < incomingBlocks){
+    totalReceived = 0;
+    while (totalReceived < blockSize){
+      iterReceived = Serial.readBytes((char *)inBuffer + totalReceived, blockSize - totalReceived);
+      if (iterReceived == 0){
+        while (!Serial.available()) ;
+      }   
+      totalReceived += iterReceived;
+    }
+
+    receivedBlocks++;
+
+    for (int i = 0; i < blockSize; i = i + 16){
+      memcpy(tempiBuffer, inBuffer + i, 16);
+      switch (mode){
+      case enc:                 
+        aes_cbc_encrypt(tempiBuffer, outBuffer, 1, iv, &ac);
+        break;
+      case dec:            
+        aes_cbc_decrypt(tempiBuffer, outBuffer, 1, iv, &ac);
+        break;
+      }
+      Serial.write(outBuffer, 16); 
+    }
+  }
+  
+  digitalWrite(13, LOW);
+}
+
+
+
+
+
+
+
+
---
+++ TeensyCrypt/TeensyCrypt/Program.cs
@@ -0,0 +1,160 @@
+﻿using NDesk.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+
+namespace TeensyCrypt
+{
+    enum mode
+    {
+        enc,
+        dec
+    }
+    class Program
+    {
+        const int blockSize = 4096;
+        static void Main(string[] args)
+        {
+            bool eArg = false, dArg = false;
+            string inputFile = null, outputFile = null, portName = null;
+
+            var p = new OptionSet() {            
+                { "i=", "The input file",   v => inputFile = v },
+                { "o=", "The output file",   v => outputFile = v },
+                { "p=", "Port name",   v => portName = v },
+                { "e",  "Encrypt the file", v => eArg = v != null },
+                { "d",  "Decrypt the file",  v => dArg = v != null },
+            };
+
+            try
+            {
+                p.Parse(args);
+                if (inputFile == null || outputFile == null || portName == null)
+                {
+                    p.WriteOptionDescriptions(Console.Out);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            mode currMode;
+            if (eArg)
+                currMode = mode.enc;
+            else if (dArg)
+                currMode = mode.dec;
+            else
+                return;
+
+            try
+            {
+
+                SerialPort port;
+
+                if (File.Exists(outputFile))
+                    File.Delete(outputFile);
+
+                port = new SerialPort(portName);
+                port.Open();
+
+                port.Write(prepareSwitch(currMode), 0, 32);                
+
+                byte[] fileBytes = File.ReadAllBytes(inputFile);
+
+                FileStream fs = new FileStream(outputFile, FileMode.Append);
+                BinaryWriter bw = new BinaryWriter(fs);
+                
+                //duljina datoteke za dekriptiranje
+                int origLen = 0;
+                if (currMode == mode.enc)
+                {
+                    port.Write(BitConverter.GetBytes(fileBytes.Length), 0, 4);
+                    //čitanje i zapisivanje header-a za enkriptirani fajl
+                    while (port.BytesToRead < 20) { }
+                    byte[] encInitBuffer = new byte[20];
+                    port.Read(encInitBuffer, 0, 20);
+                    bw.Write(encInitBuffer);
+                }
+                else
+                {
+                    //daj mu len, iv
+                    port.Write(fileBytes, 0, 20);
+                    origLen = BitConverter.ToInt32(fileBytes, 0);
+                }
+                              
+                byte[] readBuffer = new byte[blockSize];
+                byte[] writeBuffer = new byte[blockSize];
+
+                Console.WriteLine("Working...");
+                var watch = Stopwatch.StartNew();  
+
+                for (int i = (currMode == mode.enc) ? 0 : 20; i < fileBytes.Length; i = i + blockSize)
+                {
+                    if ((fileBytes.Length - i) < blockSize)
+                    {
+                        Array.Clear(writeBuffer, 0, blockSize);
+                        Array.Copy(fileBytes, i, writeBuffer, 0, fileBytes.Length - i);
+                        port.Write(writeBuffer, 0, blockSize);
+                    }
+                    else
+                    {
+                        port.Write(fileBytes, i, blockSize);
+                    }
+
+                    //pročitaj rezultat
+                    while (port.BytesToRead < blockSize) { }
+                    port.Read(readBuffer, 0, blockSize);
+                    //najprije provjerava da li je u dec modu i da li se nalazi na zadnjem bloku - ako da onda reže padding na temelju orig duljine datoteke (origLen)
+                    bw.Write(((currMode == mode.dec) && (((i - 20) / blockSize) == (origLen / blockSize))) ? trimPadding(readBuffer, (i + blockSize - 20 - origLen)) : readBuffer);
+                }
+                watch.Stop();
+                Console.WriteLine("Finished in " + watch.ElapsedMilliseconds + " ms");
+
+                port.Close();
+                bw.Close();
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                string msg = e.Message;
+                while (e.InnerException != null)
+                {
+                    e = e.InnerException;
+                    msg += e.Message;
+                }
+                Console.WriteLine(msg);
+            }
+        }
+        static byte[] trimPadding(byte[] data, int len)
+        {
+            if (len > 0)
+            {
+                byte[] trimmed = new byte[data.Length - len];
+                Array.Copy(data, 0, trimmed, 0, trimmed.Length);
+                return trimmed;
+            }
+            else
+                return data;
+        }
+        static byte[] prepareSwitch(mode currMode)
+        {
+            byte[] swdata = new byte[32];
+            Array.Clear(swdata, 0, 32);
+            switch (currMode)
+            {
+                case mode.enc:
+                    swdata[0] = 0x01;
+                    break;
+                case mode.dec:
+                    swdata[0] = 0x02;
+                    break;
+            }
+            return swdata;
+        }
+    }
+}
